a4ec9b797a1cb0939fe9ce0e310197503859d63d
Sushma-Kunjangada-Arun <kunjangadaarun.s@northeastern.edu>
2025-09-15 23:47:12 -0400

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Sushma Kunjangada Arun on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
